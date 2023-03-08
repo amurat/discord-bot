3b4245664ba143b69dd162fb90fcb31847ff758a
Altay Murat <amurat@echelon-software.com>
2023-03-08 13:40:08 -0500

    implemented ping

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+import { TOKEN } from './config';
+import { onMessageCreate, onReady } from './listeners';
+
+const client = new Client({ intents : [GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages],
+    partials: [Partials.Channel],
+});
+
+onReady(client);
+onMessageCreate(client);
+
+client.login(TOKEN); 
